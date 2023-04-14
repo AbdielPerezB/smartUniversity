9f738169fb970386013c7e156fc07fc0d43ddbbf
Abdiel <abdiel2012p@gmail.com>
2023-04-13 21:06:23 -0600

    s/c

---
+++ inicio.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark"> <!-- navbar-dark -> Color dark, bg-dark ->background:dark -->
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">BUAP Inteligente</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-link " aria-current="page" href="#">Mi perfil</a>
+              <a class="nav-link " href="#">Usuarios</a>
+            </div>
+            <div class="navbar-nav ms-auto"> <!-- ms->margin start->a la izquierda, me->margin end->a la derecha -->
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                      <img src="img/perfil.jpg"  width="30" height="24" class="d-inline-block align-text-top">
+                      Mi perfil
+                    </a>
+                  </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
