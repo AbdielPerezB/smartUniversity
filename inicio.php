f97baf9d1f7b9f9b80ae36921bc43d14605928dc
Abdiel <abdiel2012p@gmail.com>
2023-04-17 16:35:10 -0600

    Correción de los js de inicio.php

--- inicio.php
+++ inicio.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,34 +8,37 @@
     <title>Iniciov2</title>
 
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <!-- My css -->
-    <link rel="stylesheet" href="css/styleInicio.css"/>
+    <link rel="stylesheet" href="css/styleInicio.css" />
     <!-- Iconos del fontawesome -->
     <script src="https://kit.fontawesome.com/66c636a4b6.js" crossorigin="anonymous"></script>
 
 </head>
+
 <body>
     <div class="main-container d-flex">
-        <?php  include "side_nav.php"; ?>
+        <?php include "side_nav.php"; ?>
 
         <!-- Content -->
         <div class="content ">
             <!-- navbarHorizontal -->
             <?php include "nav.php"; ?>
-              <div class="container">
+            <div class="container">
                 <!-- Aqui va el contenido -->
-              </div>
-        
+                
+            </div>
+
         </div>
-        
+
     </div>
-    
+
     <!-- Agregamos el js de bootstrap -->
-    <script src="js/bootstrap2.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="js/bootstrap2.bundle.min.js"> </script>
     <!-- scripts de jquery -->
-    <script src="js/jquery-3.6.4.js" integrity="sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=" crossorigin="anonymous"></script>
+    <script src="js/jquery-3.6.4.js"></script>
     <script src="js/iniciojs.js"></script>
 
 </body>
+
 </html>
